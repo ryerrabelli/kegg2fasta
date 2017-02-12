4c1a146e7d0cd6d728ef3876d22dfabe0ef4b125
Rahul <Yerrabelli>
2017-02-12 15:09:37 -0500

    Finished part 2, which was converting the keggene ids to ncbi protein ids, ncbi gene ids, and uni prot ids.

--- kegg2fasta.py
+++ kegg2fasta.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import urllib
+import urllib.request
 
 import argparse
 from bs4 import BeautifulSoup
@@ -24,6 +25,8 @@ logger = logging.getLogger('kegg2fasta')
 logger.setLevel(logging.INFO)
 
 
+website = "http://www.kegg.jp"
+
 # maybe given an id like abc1234 the output files by default are abc1234.txt and abc1234.fasta
 def main():
     parser = argparse.ArgumentParser(description='Read pathway from KEGG and generate order sheet and fasta',
@@ -32,14 +35,15 @@ def main():
     parser.add_argument('keggid', help='kegg pathway id')
     args = parser.parse_args()
 
-    kegggeneid_list = keggpathway2genes(args.keggid)
-    print(kegggeneid_list)
-    print("Gene Count: " + str(len(kegggeneid_list)))
-    '''fp_spreadsheet = open(spreadsheetfile, 'w')
-    fp_fasta = open(fastafile, 'w')
-    for id in kegggeneid_list:
-        (ncbiproteinid, ncbigeneid, uniprotid) = kegggene2xref(kegggeneid_list)
-        (symbol, name, organism) = getfromncbigene(ncbigeneid)
+    kegggeneid_url = keggpathway2genes(args.keggid)
+    #fp_spreadsheet = open(spreadsheetfile, 'w')
+    #fp_fasta = open(fastafile, 'w')
+    c=kegggeneid_url.keys()
+    for id in kegggeneid_url.keys():
+        b=kegggeneid_url[id]
+        (ncbiproteinid, ncbigeneid, uniprotid) = kegggene2xref(b)
+        print(str(id) + "\t" + str(uniprotid))
+    '''    (symbol, name, organism) = getfromncbigene(ncbigeneid)
         (aa_len, sequence) = getfromncbiprotein(ncbiproteinid)
         # here is where we add a row to the spreadsheet
         # here is where we add a record to the fasta file
@@ -70,31 +74,62 @@ def main():
         # i know the first person who wrote seqio
     '''
 
-#    kegggeneid_list = keggpathway2genes(args.keggid)
+#    kegggeneid_url = keggpathway2genes(args.keggid)
 def keggpathway2genes(keggid):
     #http://www.kegg.jp/dbget-bin/www_bget?pathway+hsa00120
-    url = "http://www.kegg.jp/dbget-bin/www_bget?pathway+"+keggid
-    import urllib.request
+    url = website+"/dbget-bin/www_bget?pathway+"+keggid
     r = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(r,"html.parser")
     ths = soup.find_all('th')
-    kegggeneid_list = []
+    kegggeneid_url = {}
     for th in ths:
         th_nobr = th.nobr
-        if th_nobr.string == "Gene": #is the header cell for the gene row
+        if th_nobr.string.lower().strip() == "gene": #is the header cell for the Gene row
             tr = th.parent #should be the tr that contains the th and the td contains a table of the genes
             geneinfo = tr.select("td table tr")
             for geneinfopart in geneinfo:
                  #only gives the links in the first column (which is the gene code). The 2nd column is the unneeded gene description
                 genecode = geneinfopart.contents[0]
                 for link in genecode.select("a"):
-                    kegggeneid_list.append(link.string)
-    return kegggeneid_list
+                    kegggeneid_url[link.string] = website+link['href']
+    return kegggeneid_url
 
 #(ncbiproteinid, ncbigeneid, uniprotid) = kegggene2xref(kegggeneid_list)
-#def kegggene2xref(kegggeneid_list):
+def kegggene2xref(kegggene_url):
+    r = urllib.request.urlopen(kegggene_url).read()
+    soup = BeautifulSoup(r,"html.parser")
+    ths = soup.find_all('th')
+    ncbiproteinid_url = None
+    ncbigeneid_url = None
+    uniprotid_url = None
+    for th in ths:
+        th_nobr = th.nobr
+        if th_nobr.string.lower().strip() == "other dbs": #is the header cell for the Other DBs row
+            tr = th.parent #should be the tr that contains the th and the td contains a table of the genes
+            otherdbinfo = tr.select("td > div > div") #gives a series of divs
+            # within the tr, there are a series of divs. The odd number divs are the left column (titles)
+            # and the even number divs are the right column (links & code)
+            infoToRead = ""
+            for div in otherdbinfo:
+                if infoToRead:
+                    link = div.a['href']
+                    id = div.a.string
+                    if infoToRead == "protein":
+                        ncbiproteinid_url= (link, id)
+                    elif infoToRead == "gene":
+                        ncbigeneid_url= (link, id)
+                    elif infoToRead == "uniprot":
+                        uniprotid_url= (link, id)
+                    infoToRead = ""
+                #if something is prepared, then it cannot simulateneously be a title for the next one
+                if div.string != None and div.string.strip() == "NCBI-ProteinID:":
+                    infoToRead = "protein"
+                elif div.string != None and div.string.strip() == "NCBI-GeneID:":
+                    infoToRead = "gene"
+                elif div.string != None and div.string.strip() == "UniProt:":
+                    infoToRead = "uniprot"
 
-
+    return (ncbiproteinid_url, ncbigeneid_url, uniprotid_url)
 
 if __name__ == "__main__":
     main()
